db4e14cc19b4386b551a9375435d1a8c7f88f77f
JavenW <513203628@qq.com>
2020-05-06 22:20:41 -0700

    Create add.js

---
+++ commands/add.js
@@ -0,0 +1,46 @@
+module.exports = {
+    name: 'add',
+    aliases: [],
+    description: 'Add into the #queue!',
+    cooldown: 0,
+    guildOnly: true,
+    args: false,
+    usage: '',
+    execute(message, args, queue, teamA, teamB, userMap) {
+        let addUser = message.mentions.users.first();
+        if(queue.includes(addUser) || teamA.includes(addUser) || teamB.includes(addUser)){
+            message.reply("You have already in the Queue or preset Team");
+        } else {
+            queue.push(addUser);
+            userMap.set(addUser, 10);
+        }
+        let count = 0;
+        let queueList = queue.map(user => {
+            let rMember = message.guild.member(user);
+            count++;
+            return '\n' + '\t' + count + ". " + ((rMember.nickname)?rMember.nickname:user.username);
+        })
+        message.channel.send("Current Queue:" + queueList);
+
+
+        if(teamA.length!=0){
+            let aCount = 0;
+            let que1List = teamA.map(user => {
+                let rMember = message.guild.member(user);
+                aCount++;
+                return '\t' + aCount + ". " + ((rMember.nickname)?rMember.nickname:user.username);
+            })
+            message.channel.send("Team A preset:" + '\n' + que1List);
+        }
+
+        if(teamB.length!=0){
+            let bCount = 0;
+            let que2List = teamB.map(user => {
+                let rMember = message.guild.member(user);
+                bCount++;
+                return '\t' + bCount + ". " + ((rMember.nickname)?rMember.nickname:user.username);
+            })
+            message.channel.send("Team B preset:" + '\n' + que2List);
+        }
+    },
+};
